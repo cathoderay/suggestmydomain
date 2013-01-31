5fbdbce61fdc981a1e60b8a0baec1bb390a6133b
Ronald Kaiser <raios.catodicos@gmail.com>
2013-01-31 21:18:03 -0200

    adding django project

---
+++ urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import patterns, include, url
+import settings
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'suggestmydomain.views.home', name='home'),
+    # url(r'^suggestmydomain/', include('suggestmydomain.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'suggestmydomain.views.home'),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': settings.MEDIA_ROOT})
+)
+
